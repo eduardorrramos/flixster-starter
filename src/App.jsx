aec2660a5a81128af091a85b8249e49c61be8cad
Eduardo Ramos <eduardoramos@meta.com>
2024-06-11 16:02:25 -0700

    populating songs now

--- src/App.jsx
+++ src/App.jsx
@@ -6,42 +6,50 @@ import SearchBar from './SearchBar'
 import NavigationBar from './NavigationBar'
 
 const App = () => {
-  const [movie, setMovie] = useState('Random Movie')
+  // const [movie, setMovie] = useState('Random Movie')
   const [movieData, setMovieData] = useState(null)
 
+
   const fetchData = async () => {
-  const apiKey = import.meta.env.VITE_API_KEY
-  const apiToken = import.meta.env.VITE_API_TOKEN
-  const response = await fetch(`https://api.themoviedb.org/3/movie/now_playing`,
-    {
-      method: 'GET',
-      headers: {
-        'Authorization': `Bearer ${apiToken}`
-     }; })
-  const readabledata= await response.json()
-  setMovieData(readabledata)
-    };
-    console.log(movieData)
+    const apiKey = import.meta.env.VITE_API_KEY
+    const apiToken = import.meta.env.VITE_API_TOKEN
+    const response = await fetch(`https://api.themoviedb.org/3/movie/now_playing`,
+      {
+        method: 'GET',
+        headers: {
+          'Authorization': `Bearer ${apiToken}`
+        }
+      })
+      console.log("eres",response)
+    const readabledata = await response.json()
+    setMovieData(readabledata);
+  };
+  console.log(movieData)
+
+  useEffect(() => {
     
-    useEffect(() => {
-      if (movie !== '') {
-        fetchData();
-      }
-    }, [movie]);
+      fetchData();
+    
+  }, []);
 
-const handleMovieChange = (newMovie) => {
-  setMovie(newMovie);
-};
+  console.log("App moviedata",movieData)
 
+  const handleMovieChange = (newMovie) => {
+    setMovie(newMovie);
+  };
+
+  if (!movieData) {
+    return <div>Loading</div>
+  }
 
   return (
-  <div className="App">
-    <h1>Movie Information Display</h1>
-   <NavigationBar/>
-    <SearchBar/> 
-    <MovieList data={movieData}/>
-  </div>
-  );
+    <div className="App">
+      <h1>Movie Information Display</h1>
+      <NavigationBar />
+      <SearchBar />
+      <MovieList data={movieData} />
+    </div>
+  )
 }
 
 export default App
--- src/MovieList.jsx
+++ src/MovieList.jsx
@@ -1,119 +1,47 @@
 import './MovieList.css';
 import React from 'react';
 import propTypes from 'prop-types';
-
 import MovieCard from './MovieCard'
+
 // import requests
 // api_key = 'ab418bdf6d93c36a6816d0ab81c2ec63'
 
-function MovieList () {
+function MovieList ({data}) {
+    
+    // console.log(data); /*just twenty movies n data */
+
+    // console.log(data.results);
+    let moviearray = (data.results);
+    // console.log(moviearray);
+    console.log(moviearray[0].title);
+    moviearray.map((movie) => {
+        console.log(movie.title);
+        return
+    });
+    
+
+  
     return (
-        <div className="cardcontainer">
+        
+        <>
+        
         <MovieCard
-        movieTitle="movieTitle"
-        imgSrc="./public/movie.png"
-        movieRating="3"
-        />
-        <MovieCard
-        movieTitle="movieTitle"
-        imgSrc="./public/movie.png"
-        movieRating="3"
-        />
-        <MovieCard
-        movieTitle="movieTitle"
-        imgSrc="./public/movie.png"
-        movieRating="3"
-        />
-        <MovieCard
-        movieTitle="movieTitle"
-        imgSrc="./public/movie.png"
-        movieRating="3"
-        />
-        <MovieCard
-        movieTitle="movieTitle"
-        imgSrc="./public/movie.png"
-        movieRating="3"
-        />
-        <MovieCard
-        movieTitle="movieTitle"
-        imgSrc="./public/movie.png"
-        movieRating="3"
-        />
-        <MovieCard
-        movieTitle="movieTitle"
-        imgSrc="./public/movie.png"
-        movieRating="3"
-        />
-        <MovieCard
-        movieTitle="movieTitle"
-        imgSrc="./public/movie.png"
-        movieRating="3"
-        />
-        <MovieCard
-        movieTitle="movieTitle"
-        imgSrc="./public/movie.png"
-        movieRating="3"
-        />
-        <MovieCard
-        movieTitle="movieTitle"
-        imgSrc="./public/movie.png"
-        movieRating="3"
-        />
-        <MovieCard
-        movieTitle="movieTitle"
-        imgSrc="./public/movie.png"
-        movieRating="3"
-        />
-        <MovieCard
-        movieTitle="movieTitle"
-        imgSrc="./public/movie.png"
-        movieRating="3"
-        />
-        <MovieCard
-        movieTitle="movieTitle"
-        imgSrc="./public/movie.png"
-        movieRating="3"
-        />
-        <MovieCard
-        movieTitle="movieTitle"
-        imgSrc="./public/movie.png"
-        movieRating="3"
-        />
-        <MovieCard
-        movieTitle="movieTitle"
-        imgSrc="./public/movie.png"
-        movieRating="3"
-        />
-        <MovieCard
-        movieTitle="movieTitle"
-        imgSrc="./public/movie.png"
-        movieRating="3"
-        />
-        <MovieCard
-        movieTitle="movieTitle"
-        imgSrc="./public/movie.png"
-        movieRating="3"
-        />
-        <MovieCard
-        movieTitle="movieTitle"
-        imgSrc="./public/movie.png"
-        movieRating="3"
-        />
-        <MovieCard
-        movieTitle="movieTitle"
-        imgSrc="./public/movie.png"
-        movieRating="3"
-        />
-        <MovieCard
-        movieTitle="movieTitle"
+        movieTitle="${movie.title}"
         imgSrc="./public/movie.png"
         movieRating="3"
         />
         
-        </div>
+       
+        </>
 
     );
+    
 }
+
+
+
+
+
 MovieList.propTypes = {
 
 };
